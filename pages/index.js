0fb39f059ee177bb698df193b87376f52748d31d
Gavin Wang <gavinwang313@gmail.com>
2023-05-01 18:21:23 -0700

    mid way through implementing dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,18 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +24,21 @@ export default function Home() {
         <p>Hello. I'm Gavin. Here is where I will be writing about what I learned in the field of machine learning!</p>
         <Link href="/about_me">Learn more about me!</Link>
 
+      </section>
+
+      <section className={`${utilStyles.headingMD} ${utilStyles.padding1px} what`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <u1 className={utilStyles.list}>
+          {allPostsData.map(post => (
+            <li className={utilStyles.listItem} key={post.id}>
+              {post.title}
+              <br />
+              {post.id}
+              <br />
+              {post.date}
+            </li>
+          ))}
+        </u1>
       </section>
     </Layout>
   );
