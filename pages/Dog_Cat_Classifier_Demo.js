941f601afc51e1ce54b155be134f04da92aad959
Gavin Wang <gavinwang313@gmail.com>
2023-05-03 22:07:39 -0700

    better css and better text

--- pages/Dog_Cat_Classifier_Demo.js
+++ pages/Dog_Cat_Classifier_Demo.js
@@ -12,6 +12,7 @@ export default function Demo() {
     async function handleFileInputChange(e) {
         const files = e.target.files;
         if (files.length > 0) {
+            setPredictionText("click \"classify image\" button to start classifying")
             const file = files[0];
             const dataUrl = await dataUrlFromFile(file);
             setImageUrl(dataUrl);
@@ -21,10 +22,14 @@ export default function Demo() {
     async function processImage() {
         // replace this with your own image processing logic
         // console.log('Processing image:', image);
-        setPredictionText("trying very hard to classify...")
-        const prediction = await classifyImage(imageUrl);
+        if (imageUrl != null) {
+            setPredictionText("trying very hard to classify...")
+            const prediction = await classifyImage(imageUrl);
         // console.log(prediction);
         setPredictionText(formatPrediction(prediction));
+        } else {
+            setPredictionText("please upload an image first before clicking the button")
+        }
     }
 
     async function dataUrlFromFile(file) {
@@ -38,7 +43,7 @@ export default function Demo() {
     }
 
     async function classifyImage(dataUrl) {
-        const jsonData = {"data": [dataUrl]}
+        const jsonData = { "data": [dataUrl] }
         const jsonDataString = JSON.stringify(jsonData);
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
@@ -57,7 +62,7 @@ export default function Demo() {
     function formatPrediction(prediction) {
         const predictionData = prediction.data[0];
         const confidence = predictionData.confidences[0]['confidence']
-        return `${predictionData.label}: ${formatAsPercentage(confidence, 2)} confidence`; 
+        return `${predictionData.label}: ${formatAsPercentage(confidence, 2)} confidence`;
     }
 
     return (
@@ -67,15 +72,11 @@ export default function Demo() {
                 <title>Dog vs. Cat Classifyer</title>
             </Head>
             <div className="">
-            <h1 className="text-[3rem] leading-[1.3] font-bold mb-4 mx-auto">Dog vs. Cat Classifier</h1>
-                <input className="max-w-[60%] h-12 mr-[1rem] bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" type="file" onChange={handleFileInputChange}/>
-                <button className="h-12 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={processImage} >Classify Image</button>
-                <div className="my-[1rem]">
-                    {imageUrl && <Image src={imageUrl} height={288} width={288} alt="" className="mx-auto rounded shadow"/>}
-                </div>
-                <div>
-                {predictionText && <h1 className="text-center">{predictionText}</h1>}
-                </div>
+                <h1 className="text-[3rem] leading-[1.3] font-bold mb-4 mx-auto">Dog vs. Cat Classifier</h1>
+                <input className="w-[45%] h-12 mr-[1rem] bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-200 rounded shadow" type="file" onChange={handleFileInputChange} />
+                <button className="w-[40%] h-12 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-200 rounded shadow" onClick={processImage} >Classify Image</button>
+                {imageUrl && <Image src={imageUrl} height={288} width={288} alt="" className="my-[1rem] mx-auto rounded-lg shadow" />}
+                {predictionText && <h1 className="my-[1rem] text-center">{predictionText}</h1>}
             </div>
         </Layout>
     );
