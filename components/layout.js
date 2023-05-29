7a4519a78750114f43a8ba7f0b6550200478ac32
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-05-29 16:35:26 +0800

    favicon emoji update

--- components/layout.js
+++ components/layout.js
@@ -3,10 +3,14 @@ import Link from 'next/link';
 
 export const siteTitle = 'gavinwang.dev';
 
-export default function Layout({ children, home}) {
+export default function Layout({ children, home }) {
   return (
     <div className="max-w-[36rem] px-[1rem] my-[3rem] mx-auto">
       <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
@@ -16,7 +20,7 @@ export default function Layout({ children, home}) {
       </Head>
 
       <main>{children}</main>
-      
+
       {!home && (
         <div className="mt-[3rem]">
           <Link href="/">← Back to home</Link>
