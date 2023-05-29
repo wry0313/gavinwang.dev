9b24dab68262a3ea7c4c4528b5dc0c6b3ac816b4
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-05-29 11:35:13 +0800

    emoji

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function Home({ allPostsData }) {
         <h2 className="text-[1.5rem] leading-[1.4]">💾 Project Demos</h2>
         <u1 className="list-none">
           <li className="mt-[1.25rem]">
-            <Link href="Dog_Cat_Classifier_Demo"> Dog vs. Cat Classifier Demo</Link>
+            <Link href="Dog_Cat_Classifier_Demo"> 🐕 Dog vs. 🐈 Cat Classifier Demo</Link>
             <br />
             <div className="text-gray-600">
               <Date dateString="2023-05-03" />
