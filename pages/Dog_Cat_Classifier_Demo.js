fa63e0834566fe499f529e111b136d2524130be3
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-05-31 13:06:27 +0800

    random ahh animation

--- pages/Dog_Cat_Classifier_Demo.js
+++ pages/Dog_Cat_Classifier_Demo.js
@@ -1,12 +1,20 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
-import { useState } from 'react';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
 
 const classificationEndpoint = "https://hf.space/embed/jph00/testing/+/api/predict/"
 
 
 export default function Demo() {
+
+    const [animationClass, setAnimationClass] = useState('');
+
+    useEffect(() => {
+        setAnimationClass('animate-fadeIn');
+    }, []);
+
     const [imageUrl, setImageUrl] = useState(null);
     const [predictionText, setPredictionText] = useState("");
 
@@ -108,29 +116,31 @@ export default function Demo() {
             <Head>
                 <title>Dog vs. Cat Classifyer</title>
             </Head>
-            <div className="flex flex-col item-center">
-                <div className="mx-auto">
-                    <h1 className=" bg-[#F5F7F7] shadow rounded-lg text-center sm:text-[3.2rem] leading-[1.3] font-bold mb-4 px-4 py-2">🐕 Dog vs. 🐈 Cat Classifier Demo</h1>
-                    <div className="flex flex-col md:flex-row items-center">
-                        <label htmlFor="fileInput"></label>
-                        <input className="hover:scale-105  duration-300 w-[250px] md:w-[70%] mb-[0.3rem] md:mb-0 text-sm md:text-base flex-1 md:mr-[0.4rem] h-12 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-200 rounded shadow" type="file" id="fileInput" onChange={handleFileInputChange} />
-                        <button className="hover:scale-105  duration-300 w-[250px] md:w-[70%] text-sm md:text-base flex-1 h-12 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-200 rounded shadow" onClick={classifyOnClick} >Classify Image</button>
+            <div className={animationClass}>
+                <div className="flex flex-col item-center">
+                    <div className="mx-auto">
+                        <h1 className=" bg-[#F5F7F7] shadow rounded-lg text-center sm:text-[3.2rem] leading-[1.3] font-bold mb-4 px-4 py-2">🐕 Dog vs. 🐈 Cat Classifier Demo</h1>
+                        <div className="flex flex-col md:flex-row items-center bg-[#F5F7F7] shadow rounded-lg px-3 py-2">
+                            <input className="hover:scale-105  duration-300 w-[250px] md:w-[70%] mb-[0.3rem] md:mb-0 text-sm md:text-base flex-1 md:mr-[0.4rem] h-12 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-200 rounded shadow" type="file" id="fileInput" onChange={handleFileInputChange} />
+                            <button className="hover:scale-105  duration-300 w-[250px] md:w-[70%] text-sm md:text-base flex-1 h-12 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-200 rounded shadow" onClick={classifyOnClick} >Classify Image</button>
+                        </div>
+                    </div>
+
+                    {imageUrl && <Image src={imageUrl} alt="selected pic" height={288} width={288} className="mt-[2rem] mx-auto rounded-lg shadow" />}
+                    {predictionText && <h1 className="text-center">{predictionText}</h1>}
+
+                    <div className="no-scrollbar flex mt-[2rem] overflow-x-scroll bg-[#F5F7F7] shadow rounded-lg py-2">
+                        <Image className="flex-none ml-2 mr-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/dog1.png')} src="/images/dog1.png" height={100} width={(300 / 168) * 100} alt="Dog 1" />
+                        <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/dog2.png')} src="/images/dog2.png" height={100} width={(316 / 159) * 100} alt="Dog 2" />
+                        <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/cat.jpeg')} src="/images/cat.jpeg" height={100} width={(225 / 225) * 100} alt="Cat 1" />
+                        <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/funny.jpeg')} src="/images/funny.jpeg" height={100} width={(310 / 163) * 100} alt="Cat or Dog? 1" />
+                        <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/funny1.jpeg')} src="/images/funny1.jpeg" height={100} width={(1200 / 1200) * 100} alt="Cat or Dog 2" />
+                        <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/funny3.jpg')} src="/images/funny3.jpg" height={100} width={(390 / 291) * 100} alt="Cat or Dog 3" />
+                        <Image className="flex-none mr-2 ml-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/fox.jpeg')} src="/images/fox.jpeg" height={100} width={(1200 / 862) * 100} alt="Fox" />
                     </div>
                 </div>
-
-                {imageUrl && <Image src={imageUrl} alt="selected pic" height={288} width={288} className="mt-[2rem] mx-auto rounded-lg shadow" />}
-                {predictionText && <h1 className="text-center">{predictionText}</h1>}
-
-                <div className="no-scrollbar flex mt-[2rem] overflow-x-scroll bg-[#F5F7F7] shadow rounded-lg py-2">
-                    <Image className="flex-none ml-2 mr-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/dog1.png')} src="/images/dog1.png" height={100} width={(300 / 168) * 100} alt="Dog 1" />
-                    <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/dog2.png')} src="/images/dog2.png" height={100} width={(316 / 159) * 100} alt="Dog 2" />
-                    <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/cat.jpeg')} src="/images/cat.jpeg" height={100} width={(225 / 225) * 100} alt="Cat 1" />
-                    <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/funny.jpeg')} src="/images/funny.jpeg" height={100} width={(310 / 163) * 100} alt="Cat or Dog? 1" />
-                    <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/funny1.jpeg')} src="/images/funny1.jpeg" height={100} width={(1200 / 1200) * 100} alt="Cat or Dog 2" />
-                    <Image className="flex-none mx-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/funny3.jpg')} src="/images/funny3.jpg" height={100} width={(390 / 291) * 100} alt="Cat or Dog 3" />
-                    <Image className="flex-none mr-2 ml-1 rounded-lg hover:scale-110 duration-300" onClick={() => handleImageClick('/images/fox.jpeg')} src="/images/fox.jpeg" height={100} width={(1200 / 862) * 100} alt="Fox" />
-                </div>
             </div>
+
         </Layout>
     );
 }
