3666ba920ac6ae4267268924107e238814ba92e3
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-06-03 15:26:33 +0800

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -3,14 +3,18 @@ import GitHubButton from "./github-button";
 import Link from "next/link";
 
 export default function Navbar() {
-  return (
-    <nav className="invisible lg:visible fixed left-0 bg-transparent backdrop-blur-sm backdrop-saturate-[0.9] flex flex-col items-start justify-center h-screen py-4 px-4">
-      <div>
-        <Link href="" className="block hover:bg-zinc-100 hover:shadow-xs hover:rounded-lg text-stone-100 font-bold text-xl mb-6"> ~/blogs </Link>
-        <Link href="" className="block  text-stone-100 font-bold text-xl mb-6"> ~/projects </Link>
-        <GitHubButton/>
-        
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="invisible lg:visible fixed left-0 bg-transparent backdrop-blur-sm backdrop-saturate-[0.9] flex flex-col items-start justify-center h-screen py-4 px-4">
+            <div>
+                <Link href="" className="block group transition duration-300 text-stone-100 font-bold text-xl mb-6"> ~/blogs
+                    <span class="block max-w-0 group-hover:max-w-[5.5ch] transition-all duration-500 h-[0.2rem] rounded bg-stone-100"></span>
+                </Link>
+                <Link href="" className="block group text-stone-100 font-bold text-xl mb-6"> ~/projects
+                    <span class="block max-w-0 group-hover:max-w-[7ch] transition-all duration-500 h-[0.2rem] rounded bg-stone-100"></span>
+                </Link>
+                <GitHubButton />
+
+            </div>
+        </nav>
+    );
 }
